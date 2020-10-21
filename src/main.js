7c60d05923c83ef2e51213c4ee01f34d4775abdf
943944089 <943944089@qq.com>
2020-10-21 17:00:17 +0800

    2-项目配置

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import "./plugins/element.js";
 
-Vue.config.productionTip = false
+// 导入 less
+import "./styles/base.less";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
